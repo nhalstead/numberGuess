e5972e2e885bcc4127984328e75ad6104fd71192
Noah Halstead <nhalstead00@gmail.com>
2017-09-26 20:26:47 -0400

    Last Commit

--- src/main/java/com/robotics/velocity/numberGuess/App.java
+++ src/main/java/com/robotics/velocity/numberGuess/App.java
@@ -1,4 +1,7 @@
 package com.robotics.velocity.numberGuess;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
  * Hello world!
@@ -6,8 +9,64 @@ package com.robotics.velocity.numberGuess;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        
+    	
+    	int totalGuess = 5;
+    	int highestValue = -1;
+    	int answer = -1;
+    	
+    	
+    	p("Max Number: ");
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String userInput = in.readLine();
+    	highestValue = pInt(userInput);
+    	
+    	Random r = new Random();
+    	answer = r.nextInt()%(highestValue + 1);
+    	answer = Math.abs(answer);
+    	//pln(answer);
+
+    	int guess = -1;
+    	while(guess != answer){
+    		p("Please Enter a number: ");
+    		guess = pInt(in.readLine());
+    	}
+    	
+    	plne("You Have got It!");
     }
+    
+    
+    
+    public static void pln(String i){
+    	System.out.println(i);
+    }
+    public static void pln(int i){
+    	System.out.println(i);
+    }
+    
+    public static void plne(String i){
+    	System.out.println(i+"\n");
+    }
+    public static void plne(int i){
+    	System.out.println(i+"\n");
+    }
+    
+    public static void p(String i){
+    	System.out.print(i);
+    }
+    public static void p(int i){
+    	System.out.print(i);
+    }
+    
+    public static int pInt(String i){
+    	try {
+    		return Integer.parseInt(i);
+    	} 
+    	catch (Exception e){
+    		return 0;
+    	}
+    }
+    
 }
